0fccf1e6351e4c9a863aa95dd3763e1527a1735c
caozhihui <caozhihui@4399inc.com>
2016-11-17 08:56:04 +0800

    数据跑通

--- routes/home.js
+++ routes/home.js
@@ -1,23 +1,27 @@
 var _config = {
-    key : "a8e66de6eba0413397da9b710a6fa022",
-    token : "04aed13e5da8bca4a0d6e9168d04bacf3bb22997ead14b04531ee270a668575f",
-    id : '4eea503d91e31d174600008f'
+    key : "687b17f408a692556a50bae7ab218651",
+    token : "eb59ff09aa165f88a0071c498ce132108c25fc370f6bb12464bc6435163765d0",
+    id : '5818768d7dba000936108b95'
+}
+
+var _url = {
+    id : 'https://api.trello.com/1/cards/'+_config.id+'/actions?fields=badges,email,checkItemStates&member_fields=Caozhihui&key='+_config.key+'&token='+_config.token,
 }
 
 var request = require('request');
 
-
+request(_url.id, function (error, response, body) {
+    console.log(body);
+    if (!error && response.statusCode == 200) {
+        body = JSON.parse(body);
+    }
+})
 
 
 module.exports = function(app){
     app.get('/',function(req,res){
         res.render('index',{title:12312});
-        request('https://api.trello.com/1/cards/'+ _config.id +'?fields=name,idList&member_fields=fullName&key=['+_config.id +']&token=['+_config.token+']', function (error, response, body) {
-            console.log(body);
-            if (!error && response.statusCode == 200) {
-                body = JSON.parse(body);
-            }
-        })
+
     });
     app.get('/home',function(req,res){
         res.render('home',{title:123});
