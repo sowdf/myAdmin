8c1891aa0676ebc4f949124bda498ec57ed4727f
czhui1993 <229555644@qq.com>
2016-11-25 07:37:59 +0800

    mysql

--- modules/register.js
+++ modules/register.js
@@ -1,11 +1,53 @@
 var connection = require('../config/server');
 
+connection.query('select * from users where find_in_set("zhangsan@gmail.com",email)', function(err, rows, fields) {
+    if(err){
+        console.log(err);
+        return false;
+    }
 
-var user={username:'zhangsan',password:'pwdzhangsan',email:'zhangsan@gmail.com',reg_time : ''};
-connection.query('insert into users set ?', user, function(err, result) {
-    if (err) throw err;
+    console.log(rows);
 
-    console.log('inserted zhangsan');
-    console.log(result);
+
     console.log('\n');
+
 });
+
+module.exports.query = function(key){
+    connection.query('select * from users where find_in_set("'+key +'","email")', function(err, rows, fields) {
+        if(err){
+            console.log(err);
+            return false;
+        }
+
+        console.log(rows);
+
+
+        console.log('\n');
+
+    });
+};
+
+
+module.exports.delete = function(){
+
+};
+
+module.exports.change = function(){
+
+};
+
+
+module.exports.add = function(){
+
+    var user={username:'zhangsan',password:'pwdzhangsan',email:'zhangsan@gmail.com',reg_time : ''};
+    connection.query('insert into users set ?', user, function(err, result) {
+        if(err){
+            console.log(err);
+            return;
+        }
+        console.log('inserted zhangsan');
+        console.log(result);
+        console.log('\n');
+    });
+}
